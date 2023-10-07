d70c09ad92e1949b25c480e89f25d61f839edd42
Чирков Михаил <mikechirkov@yandex.ru>
2023-10-07 23:06:05 +0300

    добавлена таблица на главный экран

--- lib/presentation/views/apk_list_page.dart
+++ lib/presentation/views/apk_list_page.dart
@@ -16,6 +16,8 @@ class ApkListPage extends StatefulWidget {
 class _ApkListPageState extends State<ApkListPage> {
   late ApkInfoBloc _bloc;
 
+  final _replacePatternController = TextEditingController();
+
   @override
   void initState() {
     super.initState();
@@ -27,12 +29,34 @@ class _ApkListPageState extends State<ApkListPage> {
     assert(debugCheckHasFluentTheme(context));
     final theme = FluentTheme.of(context);
     return ScaffoldPage(
-      header: const PageHeader(
-        title: Text('Fluent UI for Flutter Showcase App'),
-      ),
-      content: Column(
+      // header: const PageHeader(
+      //   title: Text('Fluent UI for Flutter Showcase App'),
+      // ),
+      content: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Expanded(
+          Flexible(
+            fit: FlexFit.tight,
+            child: SizedBox(
+              width: 300,
+              child: Column(
+                children: [
+                  TextBox(
+                    controller: _replacePatternController,
+                  ),
+                  FilledButton(
+                    onPressed: () {
+                      _bloc.add(const ApkInfoEvent.openFiles());
+                    },
+                    child: const Text('Open'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Flexible(
+            fit: FlexFit.loose,
+            flex: 3,
             child: BlocBuilder<ApkInfoBloc, ApkInfoState>(
               bloc: _bloc,
               builder: (context, state) {
@@ -42,26 +66,63 @@ class _ApkListPageState extends State<ApkListPage> {
                     listInfo = st;
                   },
                 );
-                return ListView.builder(
-                  itemCount: listInfo?.length ?? 0,
-                  itemBuilder: (context, index) {
-                    final item = listInfo![index];
-                    return ListTile(
-                      title: Text(item.toString()),
-                    );
+                // final item = listInfo![index];
+                // "",
+                // "Исходное",
+                // "Переименовано",
+                return Table(
+                  // border: TableBorder.all(),
+                  defaultVerticalAlignment: TableCellVerticalAlignment.top,
+                  columnWidths: const <int, TableColumnWidth>{
+                    0: FixedColumnWidth(64),
+                    1: FlexColumnWidth(),
+                    2: FlexColumnWidth(),
                   },
+                  children: _rows(listInfo),
                 );
               },
             ),
           ),
-          FilledButton(
-            onPressed: () {
-              _bloc.add(const ApkInfoEvent.openFiles());
-            },
-            child: const Text('Open'),
-          ),
         ],
       ),
     );
+  }
+
+  List<TableRow> _rows(List<ApkInfo>? listInfo) {
+    final rowList = <TableRow>[];
+    rowList.add(TableRow(
+      decoration: const BoxDecoration(
+      ),
+      children: <Widget>[
+        SizedBox(),
+        TableCell(
+          child: Text("Исходное"),
+        ),
+        const TableCell(
+          child: Text("Переименовано",),
+        ),
+      ],
+    ));
+    listInfo?.forEach((e) {
+      rowList.add(TableRow(
+        decoration: const BoxDecoration(
+        ),
+        children: <Widget>[
+          const TableCell(
+            child: Checkbox(
+              checked: true,
+              onChanged: null,
+            ),
+          ),
+          TableCell(
+            child: Text(e.file.toString()),
+          ),
+          const TableCell(
+            child: Text('hghghh'),
+          ),
+        ],
+      ));
+    });
+    return rowList;
   }
 }
