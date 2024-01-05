4c34fd063d70a1cee2ae5ec70e951a0c4343f7b2
Mike <iv.mike@rambler.ru>
2024-01-06 01:38:36 +0300

    добавлен портативный режим

--- lib/internal/app_assembly.dart
+++ lib/internal/app_assembly.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/foundation.dart';
 import 'package:kiwi/kiwi.dart';
 import 'package:logger/logger.dart' as l;
 import 'package:parser_apk_info/repository/aapt_util.dart';
+import 'package:path/path.dart' as path;
 import 'package:path_provider/path_provider.dart';
 import 'package:renamer_lib/model/aapt_path_util.dart';
 
@@ -11,6 +14,27 @@ import '../data/repository/database_repository.dart';
 import '../data/repository/preferences_repository.dart';
 
 class AppAssembly {
+
+  static const _portableFileName = 'portable';
+  static const _configDir = 'ApkRenamer';
+
+  static Future<bool> checkPortable(Directory current) {
+    final portableFile = File(path.join(current.path, _portableFileName));
+    return portableFile.exists();
+  }
+
+  static Future<DatabaseRepository> _initDatabase(Directory currentDir) async {
+    final isPortable = await checkPortable(currentDir);
+    final db = DatabaseRepository();
+    if(isPortable){
+      await db.init(currentDir);
+    } else {
+      final dir = await getApplicationSupportDirectory();
+      await db.init(dir);
+    }
+    return db;
+  }
+
   static Future<void> init() async {
     if (kDebugMode) {
       l.Logger.level = l.Level.trace;
@@ -21,9 +45,8 @@ class AppAssembly {
 
     final container = KiwiContainer();
 
-    final dir = await getApplicationDocumentsDirectory();
-    final db = DatabaseRepository();
-    await db.init(dir);
+    final currentDir = AaptPathUtil.getCurrentDir();
+    final db = await _initDatabase(currentDir);
 
     container
       ..registerSingleton((di) => PreferencesRepository())
